9e11bee6b803b4c6612c8f7a71465a23c942c367
namjj5 <quangnamsh123@gmail.com>
2026-08-17 01:05:59 +0700

    Update sever.js

--- Code/sever.js
+++ Code/sever.js
@@ -4,16 +4,26 @@ const path = require("path")
 const Websocket = require("ws")
 const sever = http.createServer((req,res) => {
   let filePath = "";
-
   if(req.url === "/") {
     filePath = "./index.html";
   } else {
     filePath = "." +req.url;
   }
-
 const ext = path.extname(filePath)
-
 const contentTypes = {
   ".html": "text/html",
   ".css": "text/css",
-  ".js"
+  ".js":"text/javascript"
+};
+  fs.readFile(filePath, (err, data) => {{
+    if (err) {
+      res.writeHead(404);
+      res.end("Not Found");
+      return;
+    }
+    res.writeHead(200, {
+      "content-Type";
+      contentTypes[ext] 
+  });
+  res.end(data);
+  
